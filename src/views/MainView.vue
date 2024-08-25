f6cae4e8006e543690cdc878166c1463c74917f6
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-25 20:59:49 +0200

    feat: add MainView

---
+++ src/views/MainView.vue
@@ -0,0 +1,67 @@
+<script lang="ts" setup>
+import { ref, onMounted, watch } from 'vue'
+import SearchBar from '@/components/SearchBar.vue'
+import PlanetList from '@/components/PlanetList.vue'
+import Pagination from '@/components/Pagination.vue'
+import ActionStateHandler from '@/components/ActionStateHandler.vue'
+
+import { usePlanetStore } from '@/stores/usePlanets'
+import { storeToRefs } from 'pinia'
+import { usePageNavigation } from '@/components/composables'
+
+const planetStore = usePlanetStore()
+const { planets, currentPage, totalPages, fetchState } = storeToRefs(planetStore)
+const { fetchPlanets, searchPlanets, changePage } = planetStore
+
+const { hasNextPage, hasPreviousPage, nextPage, previousPage } = usePageNavigation(
+  totalPages,
+  currentPage,
+  changePage
+)
+
+const filterText = ref<string>()
+
+const handleFetch = () => {
+  if (filterText.value) {
+    searchPlanets(filterText.value)
+  } else {
+    fetchPlanets()
+  }
+}
+
+onMounted(() => {
+  fetchPlanets()
+})
+
+watch([filterText, currentPage], ([newFilterText, newPage], [oldFilterText, oldPage]) => {
+  if (newFilterText !== oldFilterText || newPage !== oldPage) {
+    handleFetch()
+  }
+})
+</script>
+
+<template>
+  <div class="main-view">
+    <SearchBar v-model="filterText" />
+    <ActionStateHandler :state="fetchState" @retry="handleFetch">
+      <PlanetList :planets="planets" />
+      <Pagination
+        :currentPage="currentPage"
+        :totalPages="totalPages"
+        :hasPreviousPage="hasPreviousPage"
+        :hasNextPage="hasNextPage"
+        @changePage="changePage"
+        @nextPage="nextPage"
+        @previousPage="previousPage"
+      />
+    </ActionStateHandler>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.main-view {
+  width: 100%;
+  max-width: $br-desktop;
+  margin: auto;
+}
+</style>
