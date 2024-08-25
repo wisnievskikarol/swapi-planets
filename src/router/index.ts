fe92bf98c2fca083ac1dfffb892b720ce26006da
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-25 21:10:06 +0200

    feat: add NotFoundView route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/MainView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +8,8 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    }
+    },
+    { path: '/:pathMatch(.*)*', component: () => import('@/views/NotFoundView.vue') }
   ]
 })
 
