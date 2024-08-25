d5d5d1bf7d9cd5c1e73241f5ae6475f5dc274d56
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-25 21:12:40 +0200

    feat: add PlanetList

---
+++ src/components/PlanetList.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import PlanetCard from './PlanetCard.vue'
+import type { Planet } from '@/types'
+
+defineProps<{
+  planets: Planet[]
+}>()
+</script>
+
+<template>
+  <div class="planet-list">
+    <PlanetCard v-for="planet in planets" :key="planet.url" :planet="planet" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.planet-list {
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  flex-wrap: wrap;
+}
+
+@media (min-width: 375px) {
+  .planet-list {
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 50px;
+  }
+}
+</style>
