e8314915f9509975e82f573e5e97e860f572fe49
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-25 21:11:29 +0200

    feat: add SearchBar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const modelValue = defineModel<string>()
+</script>
+
+<template>
+  <input type="text" v-model="modelValue" placeholder="Search planets..." />
+</template>
+
+<style lang="scss" scoped>
+input {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+</style>
