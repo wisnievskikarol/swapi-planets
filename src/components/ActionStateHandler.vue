9f55559511236a9916eb1ea65f118bad7e877c7f
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-26 15:40:14 +0200

    feat(ActionStateHandler): add tyling

--- src/components/ActionStateHandler.vue
+++ src/components/ActionStateHandler.vue
@@ -12,16 +12,42 @@ const emit = defineEmits<{
 </script>
 
 <template>
-  <div>
+  <div class="wrapper">
     <template v-if="state.status === 'active'">
-      <p>Loading...</p>
+      <p class="text text--loading">Loading...</p>
     </template>
     <template v-else-if="state.status === 'failed'">
-      <p>Error: {{ state.error.message }}</p>
-      <button @click="emit('retry')">Retry</button>
+      <p class="text text--error">Error: {{ state.error.message }}</p>
+      <button class="button button--retry" @click="emit('retry')">Retry</button>
     </template>
     <template v-else-if="state.status === 'succeeded'">
       <slot></slot>
     </template>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.wrapper {
+  border-radius: 8px;
+
+  @media (max-width: $br-mobile) {
+    padding: 8px;
+  }
+
+  @media (min-width: $br-desktop) {
+    padding: 24px;
+  }
+}
+
+.text {
+  font-size: $font-detail;
+
+  &--loading {
+    color: $quite-black;
+  }
+
+  &--error {
+    color: $link-color;
+  }
+}
+</style>
