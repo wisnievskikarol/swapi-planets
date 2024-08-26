c26db72944a4be5e149026b2d61f6ef81190ae8b
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-26 09:51:33 +0200

    style(SearchBar): add input wrapper

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -3,15 +3,21 @@ const modelValue = defineModel<string>()
 </script>
 
 <template>
-  <input type="text" v-model="modelValue" placeholder="Search planets..." />
+  <div class="search">
+    <input class="search__input" type="text" v-model="modelValue" placeholder="Search planets..." />
+  </div>
 </template>
 
 <style lang="scss" scoped>
-input {
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
+.search {
+  margin: 10px 0 10px 0;
+
+  &__input {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
 }
 </style>
