d0634e63039291ac720e6e878b554a3e342d1f9b
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-08-26 09:40:02 +0200

    feat(App) implement logo header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import StarWars from '@/assets/img/star-wars.webp'
+</script>
 
 <template>
+  <header class="header">
+    <img class="header__img" :src="StarWars" />
+  </header>
+
   <RouterView />
 </template>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin: auto;
+
+  &__img {
+    margin: 50px 0;
+    width: 100%;
+    max-width: 400px;
+  }
+}
+</style>
